2475b98c75e809cee0e1c414692d3c540d655014
ddong8292 <ddong8292@gmail.com>
2016-07-18 17:47:38 +0900

    2016-7-18

---
+++ day1/use_os.js
@@ -0,0 +1,12 @@
+/*
+내장 모듈 중 os모듈을 사용해보기!
+*/
+var os=require("os");
+//cpu정보
+//console.log(os.cpus());
+
+//메모리정보
+//console.log(os.freemem()/1024/1024+"Mb");
+
+//플렛폼
+//console.log(os.platform());
